75c90640e70e5f911469dc15f2d644c8d6ccb667
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2025-10-03 16:01:21 -0500

    Definición de estilo en tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,49 +7,12 @@
 
          <title>Sistema Integrado de Planificación e Inversión Pública - SIPeIP 2.0 @yield('title')</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <style>
-    body {
-        font-family: 'Roboto', sans-serif;
-        background-color: #f2f2f2;
-        color: #333;
-    }
-    header, nav, footer {
-        padding: 15px;
-    }
-    header {
-        background-color: #003366;
-        color: white;
-    }
-    h1 {
-        background-color: #003366;
-        color: white;
-    }
-    nav {
-        background-color: #35e25b;
-    }
-    nav a {
-        color: white;
-        margin-right: 20px;
-        text-decoration: none;
-    }
-    nav a:hover {
-        text-decoration: underline;
-    }
-    main {
-        padding: 20px;
-    }
-    footer {
-        background-color: #ddd;
-        text-align: center;
-    }
-    </style>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
@@ -57,33 +20,12 @@
 
         {{-- Page Heading --}}
         @if (isset($header))
-            <header class="bg-white shadow">
+            <header class="bg-primary shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1>Sistema Integrado de Planificación e Inversión Pública</h1>
+                    <h1 class="text-white text-2xl font-bold">Sistema Integrado de Planificación e Inversión Pública</h1>
                 </div>
             </header>
         @endif
-
-        {{-- Barra de navegación secundaria --}}
-        <nav>
-            {{-- Enlace al dasboard siempre disponible --}}
-            <a href="{{ url('/dashboard') }}">dashboard</a>
-
-            {{-- Enlaces sólo para admin --}}
-          
-                <a href="{{ route('entidades.index') }}">Entidades</a>
-                <a href="{{ route('unidades.index') }}">Unidades</a>  
-                <a href="{{ route('planes.index') }}">Planes</a>
-                
-                <a href="{{ route('proyectos.index') }}">Proyectos</a>
-                <a href="{{ route('programas.index') }}">Programas</a>
-                <a href="{{ route('pnd.index') }}">Objetivos PND</a>
-                <a href="{{ route('objEstrategicos.index') }}">Objetivos Estratégicos</a>
-                <a href="{{ route('ods.index') }}">Objetivos ODS</a>
-               
-
-
-        </nav>
 
         {{-- Contenido principal --}}
         <main>
@@ -91,8 +33,8 @@
         </main>
     </div>
 
-    <footer>
-        <small>&copy; {{ date('2025') }} SIPeIP 2.0</small>
+    <footer class="bg-gray-200 p-4 text-center">
+        <small>&copy; {{ date('Y') }} SIPeIP 2.0</small>
     </footer>
 </body>
 </html>
